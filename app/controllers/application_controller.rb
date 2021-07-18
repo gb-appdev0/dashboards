a5ef35cc28e7f05355fb9a4c24bf8a68e4784031
gb-appdev <sbhaktha@chicagobooth.edu>
2021-07-18 03:49:08 +0000

    finished currency section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,45 @@
 class ApplicationController < ActionController::Base
+
+  def homepage
+
+    render("homepage/index.html.erb")
+
+  end
+
+  def forex
+
+    url = "https://api.exchangerate.host/symbols"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @curr_collection = parsed_data.fetch("symbols").keys
+
+    render("forex_templates/forex.html.erb")
+
+  end
+
+  def foreign
+
+    url = "https://api.exchangerate.host/symbols"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @curr_collection = parsed_data.fetch("symbols").keys
+
+    @foreign = params.fetch("foreign")
+
+    render("forex_templates/foreign.html.erb")
+  end
+
+  def domestic
+
+    @foreign = params.fetch("foreign")
+    @domestic = params.fetch("domestic")
+
+    url = "https://api.exchangerate.host/convert?from=" + @foreign + "&to=" + @domestic
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @conversion = parsed_data.fetch("result")
+
+    render("forex_templates/domestic.html.erb")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/", { :controller => "application", :action => "homepage"})
+
+  get("/forex", { :controller => "application", :action => "forex"})
+
+  get("/forex/:foreign", { :controller => "application", :action => "foreign"})
+
+  get("/forex/:foreign/:domestic", { :controller => "application", :action => "domestic"})
+
 end
